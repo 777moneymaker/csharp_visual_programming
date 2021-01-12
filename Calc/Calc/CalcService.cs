90257db25c9b9578d55560f1c8818e8ad44c19c2
777moneymaker <adeeeq77@gmail.com>
2021-01-12 13:17:46 +0100

    Services

---
+++ Calc/Calc/CalcService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calc {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class CalcService : ICalcService {
+        double ICalcService.Add(double n1, double n2) => n1 + n2;
+        double ICalcService.Divide(double n1, double n2) => n1 / n2;
+        double ICalcService.Multiply(double n1, double n2) =>  n1 * n2;
+        double ICalcService.Subtract(double n1, double n2) => n1 - n2;
+    }
+}
---
+++ Calc/Calc/ICalcService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calc {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalcService {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Calc.ContractType".
+    [DataContract]
+    public class Calc {
+        [DataMember]
+        public double n1 { get; set; }
+        [DataMember]
+        public double n2 { get; set; }
+    }
+}
