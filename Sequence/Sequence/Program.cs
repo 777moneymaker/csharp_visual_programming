2d53daaf0217961b66a249379c2d8e9f96033df0
777moneymaker <adeeeq77@gmail.com>
2021-01-18 21:50:02 +0100

    Seq service

---
+++ Sequence/Sequence/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace Sequence {
+    [ServiceContract]
+    public interface ISequenceService {
+        [OperationContract]
+        string Translate(string seq);
+    }
+
+    public class SequenceService : ISequenceService {
+
+        public string Translate(string seq) {
+            try {
+                var parser = new Bio.IO.FastA.FastAParser();
+                byte[] byteArray = Encoding.UTF8.GetBytes(seq);
+                MemoryStream stream = new MemoryStream(byteArray);
+
+                Bio.Sequence result = parser.Parse(stream).First() as Bio.Sequence;
+                var transcribed = Bio.Algorithms.Translation.Transcription.Transcribe(result);
+                var translated = Bio.Algorithms.Translation.ProteinTranslation.Translate(transcribed) as Bio.Sequence;
+
+                return translated.ConvertToString();
+            } catch {
+                return "Invalid operation. Check your sequence. Check your format (must be FASTA).";
+            }
+
+
+        }
+    }
+
+    class Program {
+        static Uri baseAddress = new Uri("http://localhost:8080/seq");
+        static void Main(string[] args) {
+            using (ServiceHost host = new ServiceHost(typeof(SequenceService), baseAddress)) {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+}
---
+++ Sequence/SequenceClient/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using SequenceClient.SequenceService;
+using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SequenceClient {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        SeqClient seqClient;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            seqClient = new SeqClient();
+        }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e) {
+            string seq = InputSequenceTextBox.Text.Clone() as string;
+            var result = seqClient.Translate(seq);
+            OutputSequenceTextBox.Text = result;
+        }
+
+    }
+
+    public class SeqClient : System.ServiceModel.ClientBase<ISequenceService>, ISequenceService {
+        public SeqClient() { }
+
+        public SeqClient(string endpointConfigurationName) :
+                base(endpointConfigurationName) { }
+
+        public SeqClient(string endpointConfigurationName, string remoteAddress) :
+                base(endpointConfigurationName, remoteAddress) { }
+
+        public SeqClient(string endpointConfigurationName,
+            System.ServiceModel.EndpointAddress remoteAddress) :
+                base(endpointConfigurationName, remoteAddress) { }
+
+        public SeqClient(System.ServiceModel.Channels.Binding binding,
+            System.ServiceModel.EndpointAddress remoteAddress) :
+                base(binding, remoteAddress) { }
+
+        public string Translate(string seq) {
+            try {
+                return base.Channel.Translate(seq);
+            } catch (EndpointNotFoundException) {
+                return "Service is not running.";
+            }
+
+        }
+
+        public Task<string> TranslateAsync(string seq) {
+            throw new NotImplementedException();
+        }
+    }
+}
